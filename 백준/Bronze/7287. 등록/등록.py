740288c24b14ce48d3bba80a9471e17d7c07559d
정우 <108061510+jwson-automation@users.noreply.github.com>
2022-08-11 12:14:01 +0900

    외부모듈사용정답업데이트

--- 백준/Bronze/7287. 등록/등록.py
+++ 백준/Bronze/7287. 등록/등록.py
@@ -1,2 +1,19 @@
 print("33")
 print("sonjungwoo9")
+
+#외부모듈을 사용한 정답은 아래입니다.
+
+import requests
+import bs4
+
+myId = "sonjungwoo9"
+
+URL = "https://www.acmicpc.net/user/"+ myId
+
+response = requests.get(URL)
+
+soup = bs4.BeautifulSoup(response.text,"html.parser")
+
+
+print(soup.find('div').find('span',{'id':'u-solved'}).text)
+print(myId)
