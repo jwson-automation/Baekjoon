c4cde3ef44c993169cf1d8289fc8e582f3168372
정우 <108061510+jwson-automation@users.noreply.github.com>
2022-08-21 21:14:07 +0900

    [Silver I] Title: 내려가기 2, Time: 312 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15645. 내려가기 2/내려가기 2.py
+++ 백준/Silver/15645. 내려가기 2/내려가기 2.py
@@ -1,32 +1,20 @@
-from copy import deepcopy
+import sys
+input = sys.stdin.readline
 
-A = int(input())
-numbers = [ [] for _ in range (0,A)]
+min_a = min_b = min_c = 0
+max_a = max_b = max_c = 0
 
+n = int(input())
 
-for i in range (0, A):
-    B,C,D = map(int,input().split())
-    numbers[i].append(B)
-    numbers[i].append(C)
-    numbers[i].append(D)
+for i in range(n):
+    a, b, c = map(int, input().split())
 
-numbers2 = deepcopy(numbers)
+    min_a, min_b, min_c = (min(min_a, min_b) + a, 
+                          min(min_a, min_b, min_c) + b, 
+                          min(min_b, min_c) + c)
 
+    max_a, max_b, max_c = (max(max_a, max_b) + a, 
+                          max(max_a, max_b, max_c) + b, 
+                          max(max_b, max_c) + c)   
 
-for x in range (1, A):
-    numbers[x][0] = max(numbers[x-1][0], numbers[x-1][1]) + numbers[x][0]
-
-    numbers[x][1] = max(numbers[x-1][0], numbers[x-1][1], numbers[x-1][2]) +  numbers[x][1]
-
-    numbers[x][2] = max(numbers[x-1][1], numbers[x-1][2]) + numbers[x][2]
-
-
-
-for x in range (1, A):
-    numbers2[x][0] = min(numbers2[x-1][0], numbers2[x-1][1]) + numbers2[x][0]
-
-    numbers2[x][1] = min(numbers2[x-1][0], numbers2[x-1][1], numbers2[x-1][2]) +  numbers2[x][1]
-
-    numbers2[x][2] = min(numbers2[x-1][1], numbers2[x-1][2]) + numbers2[x][2]
-
-print(max(numbers[A-1][0],numbers[A-1][1],numbers[A-1][2]), min(numbers2[A-1][0],numbers2[A-1][1],numbers2[A-1][2]))
+print(max(max_a, max_b, max_c), min(min_a, min_b, min_c)) 
