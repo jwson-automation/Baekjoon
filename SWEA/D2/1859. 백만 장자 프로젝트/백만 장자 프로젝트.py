a975fd63b9bba0f6e546c47307481332f36060c3
정우 <108061510+jwson-automation@users.noreply.github.com>
2022-09-03 00:57:32 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,441 ms, Memory: 240,044 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -0,0 +1,24 @@
+# import sys
+# input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+
+for k in range(n):
+    day = int(input())
+    price = deque(list(map(int,input().split())))
+    price.reverse()
+    max = price[0]
+    answer = 0
+    
+    while price:
+        if price[0] >= max:
+            max = price[0]
+            price.popleft()
+
+        elif price [0] < max:
+            answer += max - price[0]
+            price.popleft()
+    
+    num = str(k+1)
+    print(f'#{num} {answer}')
