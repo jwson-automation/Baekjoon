cdb46a6b5dc2ceedf9f2c662582d6a44d27512bc
정우 <108061510+jwson-automation@users.noreply.github.com>
2022-09-22 14:03:49 +0900

    update sample.py

--- 백준/sample.py
+++ 백준/sample.py
@@ -1,24 +1,47 @@
-# import sys
-# input = sys.stdin.readline
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-n = int(input())
+hills = int(input())
+elevation = []
+for i in range (hills):
+    elevation.append([int(input()), int(i)])
 
-for k in range(n):
-    day = int(input())
-    price = deque(list(map(int,input().split())))
-    price.reverse()
-    max = price[0]
-    answer = 0
+count = deque([0 for _ in range (hills)])
+
+def sorting():
+    return elevation.sort(reverse=True, key=lambda x : x[0])
+
+def value():
+    tmp = []
+    for i in range(len(elevation)):
+        tmp.append(elevation[i][0])
+    tmp_max = max(tmp)
+    tmp_min = min(tmp)
+    return tmp.count(tmp_min) >= tmp.count(tmp_max)
     
-    while price:
-        if price[0] >= max:
-            max = price[0]
-            price.popleft()
+maxValue = 100
+minValue = 5
 
-        elif price [0] < max:
-            answer += max - price[0]
-            price.popleft()
+while (maxValue - minValue) > 17 :
+    sorting()
+    if value():
+        elevation[0][0] -= 1
+        count[elevation[0][1]] += 1
+        sorting()
+    else :
+        elevation[-1][0] += 1
+        count[elevation[-1][1]] += 1
+        sorting()
+    maxValue = (max(elevation, key=lambda x: x[0]))[0]
+    minValue = (min(elevation, key=lambda x: x[0]))[0]
+
+answer = 0
+while count:
+    k = count.popleft()
+    if k != 0:
+        answer += k**2
+
+print(answer)
     
-    num = str(k+1)
-    print(f'#{num} {answer}')
+
