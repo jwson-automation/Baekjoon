3fbfbe87a77bd67e3a9ae4a4e75e79981983df46
정우 <108061510+jwson-automation@users.noreply.github.com>
2022-12-26 21:16:06 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 1){
+                b++;}
+            if(num_list[i] % 2 == 0){
+                a++;}                   
+        }
+        
+        int[] answer = {a, b};
+        return answer;
+    }
+}
