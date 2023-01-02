46cb0e88252b490132d3aa452442cb3383838a02
정우 <108061510+jwson-automation@users.noreply.github.com>
2023-01-02 21:28:26 +0900

    [Silver V] Title: 단어 정렬, Time: 192 ms, Memory: 118756 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,16 +1,14 @@
+n = int(input())
+
 words = []
-for _ in range(int(input())):
-    tmp = input()
-    tmp_l = len(tmp)
-    words.append((tmp_l, tmp))
 
-words.append((100000000,'dummy'))
+for i in range(n):
+    words.append(input())
+
+setw = set(words)
+words = list(setw)
 words.sort()
-# print(words)
+words.sort(key=len)
 
-for i in range(len(words)-1):
-    if words[i] == words[i+1]:
-        continue
-    else :
-        print(words[i][1])
-
+for i in (words):
+    print(i)
