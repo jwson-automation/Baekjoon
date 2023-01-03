15cd8dfbaefa0b4a974f8a2724466ea256940a7b
정우 <108061510+jwson-automation@users.noreply.github.com>
2023-01-03 20:21:08 +0900

    [Bronze I] Title: 이항 계수 1, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+A,B = map(int,input().split())
+
+def factorial(n):
+    ans = 1
+    for i in range(2, n+1):
+        ans *= i
+    return ans
+
+def bino_coef_factorial(n, r):
+    return factorial(n) // factorial(r) // factorial(n-r)
+
+print(bino_coef_factorial(A, B))
